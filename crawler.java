fe332f8c01d6d6775f8264396b4660d113428dce
Malek-2002 <89798604+Malek-2002@users.noreply.github.com>
2023-03-18 20:45:24 +0200

    Crawler File

---
+++ crawler.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+
+public class crawler
+{
+    public static void main(String args[])
+    {
+
+        try{
+        Document doc = Jsoup.connect("https://codeforces.com").get();
+        String title = doc.title();
+        System.out.println("title: " + title);
+        //String S = doc.text();
+        
+        //System.out.println(S.split("Premier League",-1).length - 1) ;
+        
+        Elements links =  doc.select("a[href]");
+        
+        for(Element link: links)
+        {
+            System.out.println("\nlink :" + link.attr("href"));
+           //System.out.println("text: " + link.text());
+        }
+
+        }
+        catch(IOException ex)
+        {
+            System.out.println("A7a");
+        }
+
+    }
+}
