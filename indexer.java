15b9300f7cd078b8134ca35de2741600a3c2a440
Kareem Alaa Abdullah <96543783+KareemAlaa1@users.noreply.github.com>
2023-04-17 17:38:46 +0200

    stem and stop word
    How to install opennlp
    https://www.tutorialspoint.com/opennlp/opennlp_environment.htm

--- indexer.java
+++ indexer.java
@@ -3,11 +3,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
+import java.util.Scanner;
+
+import opennlp.tools.stemmer.PorterStemmer;
 
 
 class kareem {
@@ -21,32 +23,17 @@ class kareem {
 
 
 public class indexer implements Runnable {
-
-    //static ArrayList<url_tag> table;
     Mongod mongodb;
-    ArrayList<String> stopWords;
+    PorterStemmer stemming;
 
     public indexer(Mongod mongo) {
         synchronized (mongo.lock) {
             mongodb = mongo;
         }
-
-        //this.table = ar;
-        stopWords = new ArrayList<>();
-        stopWords.add("a");
-        stopWords.add("about");
-        stopWords.add("above");
-        stopWords.add("actually");
-        stopWords.add("after");
-        stopWords.add("again");
-        stopWords.add("against");
-        stopWords.add("all");
-// test
-        // main();
+        stemming = new PorterStemmer();
     }
 
-    public void main( ArrayList<url_tag> table)
-    {
+    public void main(ArrayList<url_tag> table) throws FileNotFoundException {
         for (int i = 0; i < table.size(); i++) {
             url_tag tempTable = table.get(i);
             Integer tempID = tempTable.id;
@@ -58,7 +45,8 @@ public class indexer implements Runnable {
 
             String tempWord = "";
             for (int j = 0; j < tempContent.length() + 1; j++) {
-                if (j == tempContent.length() || tempContent.charAt(j) == ' ') {
+                if ((j == tempContent.length() || tempContent.charAt(j) == ' ') && !isStopWord(tempWord)) {
+                    tempWord = stemming.stem(tempWord);
                     if (idx.containsKey(tempWord)) {
                         Integer cnt = idx.get(tempWord);
                         idx.put(tempWord, cnt + 1);
@@ -89,15 +77,26 @@ public class indexer implements Runnable {
 
 
         }
-
-        //Set<String> s = idx.keySet();
-
-        //System.out.println(s);
+        
     }
 
 
     @Override
     public void run() {
         //main();
+    }
+
+    public boolean isStopWord(String s) throws FileNotFoundException {
+        File file = new File("stopwords.txt");
+        boolean check = false;
+        final Scanner scanner = new Scanner(file);
+        while (scanner.hasNextLine()) {
+            final String lineFromFile = scanner.nextLine();
+            if (lineFromFile == s) {
+                check = true;
+                break;
+            }
+        }
+        return check;
     }
 }
